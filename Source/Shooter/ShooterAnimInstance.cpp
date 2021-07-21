e2cee80a47eb4fe4a894734a22dd7c0ed21a46e8
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-07-21 02:06:47 -0300

    Movement Offset Yaw

--- Source/Shooter/ShooterAnimInstance.cpp
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -3,6 +3,7 @@
 #include "ShooterAnimInstance.h"
 #include "ShooterCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 {
@@ -29,6 +30,19 @@ void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
         {
             bIsAccelerating = false;
         }
+
+        FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
+        FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
+        MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator( MovementRotation, AimRotation).Yaw;
+        
+        // FString RotationMessage = FString::Printf(TEXT("Base Aim Rotation: %f"), AimRotation.Yaw);
+        // FString MovementRotationMessage = FString::Printf(TEXT("MovementRotation: %f"), MovementRotation.Yaw);
+        // FString MovementOffsetYawMessage = FString::Printf(TEXT("MovementOffsetYaw: %f"), MovementOffsetYaw);
+        
+        // if(GEngine)
+        // {
+        //     GEngine->AddOnScreenDebugMessage(1, 0.f, FColor::White, MovementOffsetYawMessage);
+        // }
     }
 }
 
--- Source/Shooter/ShooterAnimInstance.h
+++ Source/Shooter/ShooterAnimInstance.h
@@ -34,5 +34,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta =(AllowPrivateAccess = "true"))
 	bool bIsAccelerating;
+
+	//Offset Yaw used for strafing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Movement, meta =(AllowPrivateAccess = "true"))
+	float MovementOffsetYaw;
 	
 };
