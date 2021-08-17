e969e4a74b71a7a6839f2612b39b1b4ecf105218
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-08-16 23:01:16 -0300

    Item interpolation variables

--- Source/Shooter/Item.cpp
+++ Source/Shooter/Item.cpp
@@ -13,7 +13,12 @@ AItem::AItem():
 	ItemName(FString("Default")),
 	ItemCount(0),
 	ItemRarity(EItemRarity::EIR_Common),
-	ItemState(EItemState::EIS_Pickup)
+	ItemState(EItemState::EIS_Pickup),
+	//Item interp
+	ZCurveTime(0.7f),
+	ItemInterpStartLocation(FVector(0.f)),
+	CameraTargetLocation(FVector(0.f)),
+	bInterping(false)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -195,9 +200,33 @@ void AItem::SetItemProperties(EItemState State)
 	}
 }
 
+
 void AItem::SetItemState(EItemState State)
 {
 	ItemState = State;
 	SetItemProperties(State);
 }
 
+void AItem::StartItemCurve(AShooterCharacter* Char)
+{
+	//store a handle to the character
+	Character = Char;
+	//store initial location of the item
+	ItemInterpStartLocation = GetActorLocation();
+	bInterping = true;
+	SetItemState(EItemState::EIS_EquipInterping);
+
+	GetWorldTimerManager().SetTimer(
+		ItemInterpTimer,
+		this,
+		&AItem::FinishInterping,
+		ZCurveTime);
+}
+
+void AItem::FinishInterping()
+{
+	if (Character)
+	{
+		Character->GetPickupItem(this);
+	}
+}
--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -106,6 +106,32 @@ private:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
 	class UCurveFloat* ItemZCurve;
 
+	//Starting location when interping begins
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
+	FVector ItemInterpStartLocation;
+
+	//Target interp location in front of the camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
+	FVector CameraTargetLocation;
+
+	//true when is interping
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
+	bool bInterping;
+
+	//Plays when start interping
+	FTimerHandle ItemInterpTimer;
+
+	//Duration of the curve and timer
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
+	float ZCurveTime;
+
+	//Pointer to the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Item Properties", meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* Character;
+
+	//Called when item interp timer is finished
+	void FinishInterping();
+
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const {return PickupWidget;};
 	FORCEINLINE USphereComponent* GetAreaSphere() const {return AreaSphere;};
@@ -113,4 +139,7 @@ public:
 	FORCEINLINE EItemState GetItemState() const {return ItemState;};
 	void SetItemState(EItemState State);
 	FORCEINLINE USkeletalMeshComponent* GetItemMesh() const {return ItemMesh;};
+
+	//Called from AShooterCharacter class
+	void StartItemCurve(AShooterCharacter* Char);
 };
