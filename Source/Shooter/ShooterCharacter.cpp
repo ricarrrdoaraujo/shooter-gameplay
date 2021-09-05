444648d30b28a8bee5b215156b8914612939cb05
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-09-05 19:27:46 -0300

    Ammo types and ammo widget

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -59,7 +59,10 @@ AShooterCharacter::AShooterCharacter() :
 	bShouldTraceForItems(false),
 	//Camera interp location variables
 	CameraInterpDistance(250.f),
-	CameraInterpElevation(65.f)
+	CameraInterpElevation(65.f),
+	//Starting ammo amount
+	Starting9mmAmmo(85),
+	StartingARAmmo(120)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -99,6 +102,8 @@ void AShooterCharacter::BeginPlay()
 	}
 	//Spawn the default weapon and equip it
 	EquipWeapon(SpawnDefaultWeapon());
+
+	InitializeAmmoMap();
 }
 
 void AShooterCharacter::MoveForward(float Value)
@@ -641,4 +646,10 @@ void AShooterCharacter::SwapWeapon(AWeapon* WeaponToSwap)
 	EquipWeapon(WeaponToSwap);
 	TraceHitItem = nullptr;
 	TraceHitItemLastFrame = nullptr;
+}
+
+void AShooterCharacter::InitializeAmmoMap()
+{
+	AmmoMap.Add(EAmmoType::EAT_9mm, Starting9mmAmmo);
+	AmmoMap.Add(EAmmoType::EAT_AR, StartingARAmmo);
 }
--- Source/Shooter/ShooterCharacter.h
+++ Source/Shooter/ShooterCharacter.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Character.h"
 #include "ShooterCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class EAmmoType : uint8
+{
+    EAT_9mm	UMETA(DisplayName = "9mm"),
+	EAT_AR	UMETA(DisplayName = "AR"),
+
+	EAT_MAX	UMETA(DisplayName = "DefaultMAX"),
+};
+
 UCLASS()
 class SHOOTER_API AShooterCharacter : public ACharacter
 {
@@ -95,6 +104,9 @@ protected:
 
 	//Drops currently equipped weapon and equips TraceHitItem
 	void SwapWeapon(AWeapon* WeaponToSwap);
+
+	// initialize ammo map with ammo values
+	void InitializeAmmoMap();
 	
 public:	
 	// Called every frame
@@ -258,6 +270,18 @@ private:
 	// Distance upward from the camera for the interp destination
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Items, meta = (AllowPrivateAccess = "true"))
 	float CameraInterpElevation;
+
+	//Map to keep track of ammo of the different ammo types
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Items, meta = (AllowPrivateAccess = "true"))
+	TMap<EAmmoType, int32> AmmoMap;
+
+	//Starting amount of 9mm ammo
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Items, meta = (AllowPrivateAccess = "true"))
+	int32 Starting9mmAmmo;
+
+	//Starting amount of AR ammo
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Items, meta = (AllowPrivateAccess = "true"))
+	int32 StartingARAmmo;
 	
 public:
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; };
